f064a41ff4a33a82b902f5feec5b5e54484d816b
harshkr212 <guptagamer8@gmail.com>
2025-12-15 12:09:43 +0530

    build completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import Navbar2 from './components/Navbar2'
 import WorkSpace from './components/WorkSpace'
 import CodeState from './CodeStore/CodeState'
-import Navbar1 from './components/Navbar1'
+
 
 function App() {
   
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
     <CodeState>
-      {/* <Navbar1/> */}
+     
     <Navbar2/>
    
       
